0d479ae27ff7862d68c30e6a72a1a371da237356
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2022-04-28 17:07:43 +0100

    Delete 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    i = 0
-    if len(argv) > 2:
-        for j in range(1, len(argv)):
-            i += int(argv[c])
-    print(f"{i}")
