cf1d31336023e97c49149557385302e49a22f55d
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-05-17 09:20:25 -0700

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Creating a class named Square"""
 
-    class square:
-         """A class named Square
+
+class Square:
+     """A class named Square
     Attributes:
     attr1 (size): size of square
     """
 
-        def __init__(self, size):
-            """
+    def __init__(self, size):
+         """
         Args:
         size: size for __size attribute of class instance
         """
-            self.__size = size
+        self.__size = size
