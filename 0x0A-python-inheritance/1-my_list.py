03a327f9aad34bc7f97053e4ac408796e6923969
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-05-30 07:40:08 -0700

    trying

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"A class named MyList"
+
+class MyList(list):
+    """A class named MyList"""
+    def print_sorted(self):
+        """Prints instance"""
+        print(sorted(self))
