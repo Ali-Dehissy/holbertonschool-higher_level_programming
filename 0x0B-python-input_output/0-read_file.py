688b4cc5e8cca8a18d36d50430cc3f0734ff83ff
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-05-31 13:30:06 -0700

    trying

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining the read_file function"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout"""
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
+            print(line, end="")
