d943c8741332adaa9d2544ff48cc8dc7db9200ae
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-05-31 14:24:27 -0700

    documentation update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""A function that returns the dictionary description 
-with simple data structure"""
+"""A function that returns the dictionary description"""
 
 
 def class_to_json(obj):
-    """A function that returns the dictionary description 
-    with simple data structure"""
+    """A function that returns the dictionary description"""
 
     return obj.__dict__
