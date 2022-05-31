891e87f5324e4a8faf86adf95ede57a21894ef61
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-05-31 14:06:04 -0700

    trying

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A function that returns the JSON representation of an object (string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)"""
+    json_rep = json.dumps(my_obj)
+    return json_rep#!/usr/bin/python3
+"""A function that returns the JSON representation of an object (string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)"""
+    json_rep = json.dumps(my_obj)
+    return json_rep
