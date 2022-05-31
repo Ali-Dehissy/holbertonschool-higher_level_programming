78d99704ea94d2ba9a5d49fb7c9b86a38eb7da71
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2022-05-31 21:43:11 +0100

    Delete 10-class_to_json.py

--- 0x0B-python-input_output/10-class_to_json.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Creating a function class_to_json that return a JSON from a class"""
-
-
-def class_to_json(obj):
-    """Returns the dictionary description of object"""
-    
-    return obj.__dict__
