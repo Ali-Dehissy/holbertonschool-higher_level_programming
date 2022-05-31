f32ca95101ffe383b429086dd2085eef9f94a543
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2022-05-31 21:58:53 +0100

    Delete 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Defining the append_write function"""
-
-
-def append_write(filename="", text=""):
-    """Appends a string at end of a text file, returns # of characters written"""
-    with open(filename, 'a', encoding='utf-8') as file:
-        return file.write(text)
