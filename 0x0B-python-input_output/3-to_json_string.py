49e1a958b013989f1e7631dd8f4354fa2eb3d3f5
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-05-31 13:34:45 -0700

    trying

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defining the write_file function"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file and returns number of characters written"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
