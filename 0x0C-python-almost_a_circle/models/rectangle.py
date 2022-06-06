b62043d913491d47e199c37ccb41a7c7761af9dc
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2022-06-06 23:39:27 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-"""This module creates the Rectangle class that inherits from Base"""
+"""Creating a Rectangle class that inherits from Base"""
 
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """A class named Rectangle
-    Attributes:
-    attr1(id): id of object
-    attr2(width): rectangle width
-    attr3(height): rectangle height
-    attr4(x): number of spaces before rectangle
-    attr5(y): number of newlines before rectangle
-    """
+    """A class named Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes instance of Rectangle class"""
         super().__init__(id)
