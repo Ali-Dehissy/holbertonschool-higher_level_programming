2afa25a8d664e850597156c737750f0bbeec19ba
root <ali1998dehissy@gmail.com>
2022-09-22 11:50:13 -0700

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""square"""
+
+class Square:
+    """square class    """
+
+    def __init__(self, size):
+        """Square constructor"""
+        
+        self.__size = size
