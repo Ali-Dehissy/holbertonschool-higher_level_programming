99097c4d8c3fb36e450f197814ccce79e49982b1
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-09-29 22:06:20 +0100

    basegeo

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """An class"""
+    def area(self):
+        """An exception"""
+        raise Exception("area() is not implemented")
