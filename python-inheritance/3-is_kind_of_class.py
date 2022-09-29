f36387f5a222fdb2f8be2f5757be1f9c44a2234a
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-09-29 21:06:34 +0100

    iskind

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Is kind of class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns true or flase  based on the inheritance"""
+    return isinstance(obj, a_class)
