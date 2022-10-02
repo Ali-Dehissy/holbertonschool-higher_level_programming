57ed3380cbd73745e32f0bd28da2534b756af036
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-10-02 16:13:23 +0100

    rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """BaseGeometry"""
+    def __init__(self, width, height):
+        """Initialising instance"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
