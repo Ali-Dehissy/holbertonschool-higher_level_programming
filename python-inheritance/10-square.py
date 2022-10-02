857f2f8c7974a5cc1a7f8415fe2b0c2c76d7c3da
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-10-02 16:29:37 +0100

    square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A Square"""
+    def __init__(self, size):
+        """Initialising instance"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size
