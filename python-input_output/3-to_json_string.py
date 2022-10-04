b2ee25c7d2ff07ddd12db2a6aedc5505cb21c3ba
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-10-04 20:33:57 +0100

    tojson

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""To_json_string function"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an obj string"""
+    return json.dumps(my_obj)
