157e5660b47d04ea3d659ca61602cbe0a5e9c472
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-10-09 20:37:19 +0100

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,39 @@ class Rectangle(Base):
                                                        self.y,
                                                        self.width,
                                                        self.height))
+    def update(self, *args, **kwargs):
+        """Updateing instance using setter method"""
+        place = 0
+        if (args is not None) and (len(args) > 0):
+            for arg in args:
+                if place == 0:
+                    self.id = arg
+                elif place == 1:
+                    self.width = arg
+                elif place == 2:
+                    self.height = arg
+                elif place == 3:
+                    self.x = arg
+                elif place == 4:
+                    self.y = arg
+                else:
+                    break
+                place += 1
+
+        elif kwargs is not None:
+            for (key, value) in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """Returns dictionnary of class rectangle"""
+        return ({'id': self.id, 'width': self.width, 'height': self.height,
+                 'x': self.x, 'y': self.y})
