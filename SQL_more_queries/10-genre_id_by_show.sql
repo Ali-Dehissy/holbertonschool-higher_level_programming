bdfb8dc808f6729bff51dca17f0d3c8f48709d61
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2022-11-09 13:13:16 +0100

    Update 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Listing all tvshows contained in the server that has at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
+  FROM tv_shows
+  RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+  ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
