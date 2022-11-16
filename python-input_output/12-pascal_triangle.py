c6e1dfb32d3223be7b6acb9656d806b3e1f6eed5
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-11-16 17:55:12 +0100

    triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,14 +3,13 @@
 
 def pascal_triangle(n):
     """Returning a list of integers representing the triangle"""
+    var = []
     if n <= 0:
-        return []
-    pascal = []
-    row = []
-    for j in range(n):
-        row = [1]
-        if j > 0:
-            for k in range(i):
-                row.append(sum(pascal[-1][k:k+2]))
-        pascal.append(row)
-    return pascal
+        return var
+    if n == 1:
+        return [[1]]
+
+    var = [[1]]
+    for i in range(n-1):
+        var.append([a+b for a, b in zip([0] + var[-1], var[-1] + [0])])
+    return var
