032100ffd2347b0c7760aa9474cf226b0acf9efd
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-11-17 22:03:55 +0100

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,14 +2,29 @@
 """Base class module"""
 
 
+import json
+import csv
+
+
 class Base:
     """Base class"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Base class initialize"""
-        if id is not None:
-            self.id = id
-        else:
+        """Creating object from a JSON file"""
+
+        if id is None:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = self.__nb_objects
+        else:
+            self.id = id
+
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returning the JSON string"""
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
