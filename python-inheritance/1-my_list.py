6c60ae4c4f7856cc8579e2d3aa3d11bf43a5c1b1
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2022-11-17 13:52:55 +0100

    update

--- python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""A module class Mylist"""
-
-
-class MyList(list):
-    """Mylist class"""
-    def print_sorted(self):
-        """Printing instance"""
-        print(sorted(self))
