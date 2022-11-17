516d37820a84e5935be616d9aa44365ae248dad7
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-11-17 14:08:49 +0100

    .

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Creating a class named Square"""
-
-
+'''a module'''
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """A class named Square"""
+    '''This is a class'''
+
     def __init__(self, size):
-        """Initializing an instance"""
-        self.integer_validator("size", size)
+        '''a function'''
+        self.integer_validator('size', size)
         self.__size = size
-        super().__init__(size, size)
+
+    def area(self):
+        '''a function'''
+        return self.__size ** 2
 
     def __str__(self):
-        """Returns informal string representation"""
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        '''a function'''
+        return f"[Square] {self.__size}/{self.__size}"
