3d195ad8aa39f787faf802bd4ca4eea904deec71
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2022-11-18 00:02:21 +0100

    module doc

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Square"""
+"""Square class"""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """A Square"""
+    """Class square"""
     def __init__(self, size):
-        """Initialising instance"""
+        """Initializing an instance"""
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size
+        super().__init__(size, size)
