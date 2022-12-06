0cfcd484e0f9df48b65914aef3f7cc9bf1d51b92
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-12-06 21:23:12 +0100

    yaaaaaa rabi

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let j;
-for (j = 0; j < strings.length; j++){
+for (j = 0; j < strings.length; j++) {
   console.log(strings[j]);
 }
