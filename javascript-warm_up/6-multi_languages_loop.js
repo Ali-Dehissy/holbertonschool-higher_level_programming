9e0dd07d80d97c11f642df9fd88847792a7262c2
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-12-06 21:20:34 +0100

    semistandrad update

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
+
 const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let j;
-for (j = 0; j < strings.length; j++){
-console.log(strings[j]);
+for (j = 0; j < strings.length; j++)
+{
+  console.log(strings[j]);
 }
